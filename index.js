3d46e571cb2d11506cd71cb4dc612aa8e058e78e
pero4l <lilpero03@gmail.com>
2025-10-15 01:45:27 +0100

    register the router and want to test it

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const authUser = require('./router/user.route')
+
 app.get('/', (req, res) => {
     res.status(200).json({
         "sucess": true,
         "message": "Loaded sucessfully"
     })
 })
+
+
+app.use('/auth', authUser)
 
 const PORT = 3000
 app.listen(PORT, ()=>{
--- router/user.route.js
+++ router/user.route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { createUser } = require('../controllers/usersController');
 
-router.post('/', createUser);
+router.post('/register', createUser);
 
 module.exports = router;
